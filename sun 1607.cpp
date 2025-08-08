7934d1756a6bea6ee5fe8e04031d715d784ac80a
sunhyunwoo <seonhyeonu9@gmail.com>
2025-08-08 12:23:50 +0900

    Add files via upload

---
+++ sun 1607.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+	int num1 = 10;
+    int num2, num3;
+    num2 = 20;
+    num3 = 30;
+    char ch;
+    ch ='A';
+    
+    printf("num1 값: %d \n", num1);
+    printf("num2 값: %d, num3 값: %d \n", num2, num3);
+    printf("ch값: %c \n", ch);
+    
+    return 0;
+}
